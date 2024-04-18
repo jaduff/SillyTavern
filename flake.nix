37a417e4f79de32546b70fa3d447545822050ffd
James Duff <jaduff@users.noreply.github.com>
2024-04-18 19:44:13 +0800

    integrated openllm & flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+        description = "AI Test";
+        inputs.flake-utils.url = "github:numtide/flake-utils";
+        inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+
+        outputs =
+        { self
+                , flake-utils
+                        , nixpkgs
+        }:
+        flake-utils.lib.eachDefaultSystem (
+                        system:
+                        let
+                        pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
+                        in
+                        {
+                        devShell = pkgs.mkShell {
+                        packages = with pkgs; [
+# See https://github.com/NixOS/nixpkgs/issues/59209.
+                        bashInteractive
+                        nodejs
+                        (pkgs.python3.withPackages (python-pkgs: [
+                                                    python-pkgs.openllm
+                        ]))
+                        ];
+                        buildInputs = with pkgs; [
+
+                        ];
+                        shellHook = ''
+                        TRUST_REMOTE_CODE=True;
+                        '';
+                        };
+
+                        }
+        );
+}
